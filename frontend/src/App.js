cce3777045d961c86a713510f4a39df799c13e29
El Linzer <elinzer5@gmail.com>
2022-11-23 11:55:34 -0500

    login functional

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,23 @@
+import { Route, Switch } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { useState, useEffect } from 'react';
+import LoginFormPage from './components/Login';
+import * as sessionActions from './store/session';
+
 function App() {
-  return (
-    <h1>Hello from App</h1>
+  const dispatch = useDispatch();
+  const [isLoaded, setIsLoaded] = useState(false);
+  useEffect(() => {
+    dispatch(sessionActions.restoreUser()).then(() => setIsLoaded(true));
+  }, [dispatch]);
+
+
+  return isLoaded && (
+    <Switch>
+      <Route path="/login">
+        <LoginFormPage />
+      </Route>
+    </Switch>
   );
 }
 
