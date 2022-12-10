d55ed75a0f00c234b141dd7564ab615c8b7a7256
El Linzer <elinzer5@gmail.com>
2022-12-10 09:08:39 -0600

    tees frontend styling

--- frontend/src/components/Tees/index.js
+++ frontend/src/components/Tees/index.js
@@ -1,14 +1,24 @@
 import Image from 'react-bootstrap/Image';
-
+import { useState, useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import './tees.css'
 
 const Tees = () => {
 
+    const tees = useSelector(state => state.tees);
+    const teeList = Object.values(tees);
 
 
 
     return (
-        <div>
-            Hi from Tees!
+        <div className='tee-display'>
+            {teeList.map((tee) => (
+                <div key={tee.id}>
+                    <Image src={tee.imageUrl} alt={tee.name} className='tee-img' fluid />
+                    <h4>{tee.name}</h4>
+                </div>
+                )
+            )}
         </div>
     )
 }
