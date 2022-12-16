06ed3d1c463591d8b3b556d40b5197226a2aa3cd
El Linzer <elinzer5@gmail.com>
2022-12-16 11:27:31 -0600

    small nav adjustment

--- frontend/src/components/Splash/index.js
+++ frontend/src/components/Splash/index.js
@@ -11,7 +11,7 @@ const Splash = () => {
                                 <h1 class="display-5 fw-bolder text-white mb-2">For all your gay tee needs</h1>
                                 <p class="lead fw-normal text-white-50 mb-4">Do you love T-shirts? Do you love the LGBTQ community? Find a shirt that speaks to you and rep the community in style!</p>
                                 <div class="d-grid gap-3 d-sm-flex justify-content-sm-center justify-content-xl-start">
-                                    <a class="btn btn-primary btn-lg px-4 me-sm-3" href="#features">Tee Time!</a>
+                                    <a class="btn btn-primary btn-lg px-4 me-sm-3" href="/tees">Tee Time!</a>
                                     <a class="btn btn-outline-light btn-lg px-4" href="/about">What is happening?</a>
                                 </div>
                             </div>
