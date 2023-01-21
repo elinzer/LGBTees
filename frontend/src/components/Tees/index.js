5f65f4857278a1fa4533def843ab4cbb982d5039
El Linzer <elinzer5@gmail.com>
2023-01-21 10:32:17 -0600

    links to tees

--- frontend/src/components/Tees/index.js
+++ frontend/src/components/Tees/index.js
@@ -1,5 +1,4 @@
 import Image from 'react-bootstrap/Image';
-import { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import './tees.css'
 
@@ -13,10 +12,10 @@ const Tees = () => {
     return (
         <div className='tee-display'>
             {teeList.map((tee) => (
-                <div key={tee.id}>
+                <div key={tee.id} className='tee-card'>
                     <Image src={tee.imageUrl} alt={tee.name} className='tee-img' fluid />
-                    <div>{tee.name}</div>
-                    <div>{tee.brand}</div>
+                    <div><a href={tee.url} target='_blank' className='tee-link'>{tee.name}</a></div>
+                    <div className='tee-brand'>{tee.brand}</div>
                     <div>${tee.price}</div>
                 </div>
                 )
