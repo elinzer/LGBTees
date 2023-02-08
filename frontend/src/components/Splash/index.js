48a9b42edee579a57f22d1333de439f3c5b4f516
El Linzer <elinzer5@gmail.com>
2023-02-08 16:37:25 -0600

    adding more to splash

--- frontend/src/components/Splash/index.js
+++ frontend/src/components/Splash/index.js
@@ -21,7 +21,27 @@ const Splash = () => {
                     </div>
                 </div>
             </header>
+            <section class="py-5 bg-light">
+                <div class="container px-5">
+                    <div class="row gx-5">
+                        <div class="col-xl-8">
+                            <h2 class="fs-4 mb-4">Don't need a shirt but still want to support your LGBTQ fam? Donate directly to one of these orgs doing critical work for the community!</h2>
+                            <div class="mb-4">
+                                <a class="link-dark" href="https://transgenderlawcenter.org/about" target={"_blank"}><h4>Transgender Law Center</h4></a>
+                                <div class='fs-5'>The largest national trans-led organization advocating for a world in which all people are free to define themselves and their futures.</div>
+                            </div>
+                            <div class="mb-4">
+                                <a class="link-dark" href="https://bravespacealliance.org/who-we-are/mission-values/" target={"_blank"}><h4>Brave Space Alliance</h4></a>
+                                <div class='fs-5'>The first Black-led, trans-led LGBTQ+ center located on the South Side of Chicago.</div>
+                            </div>
+                            <div class="mb-4">
+                                <a class="link-dark" href="#!"><h4>This is another news article headline, but this one is a little bit longer</h4></a>
+                            </div>
 
+                        </div>
+                    </div>
+                </div>
+            </section>
         </div>
     )
 
