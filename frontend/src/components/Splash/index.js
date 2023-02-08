cb6167b2b9beb8edf3637054465231729e047186
El Linzer <elinzer5@gmail.com>
2023-02-08 15:42:08 -0600

    cute LGBTees color gradient

--- frontend/src/components/Splash/index.js
+++ frontend/src/components/Splash/index.js
@@ -1,4 +1,5 @@
 import PicCarousel from "./Carousel";
+import "./Splash.css";
 
 const Splash = () => {
     return (
@@ -8,7 +9,7 @@ const Splash = () => {
                     <div class="row gx-5 align-items-center justify-content-center">
                         <div class="col-lg-8 col-xl-7 col-xxl-6">
                             <div class="my-5 text-center text-xl-start">
-                                <h1 class="display-5 fw-bolder text-white mb-2">Welcome to LGBTees!</h1>
+                                <h1 class="display-5 fw-bolder text-white mb-2">Welcome to <span className="lgbt-splash">LGBTees!</span></h1>
                                 <p class="lead fw-normal text-white-50 mb-4">Do you love T-shirts? Do you love the LGBTQ community? Find a shirt that speaks to you and rep the community in style!</p>
                                 <div class="d-grid gap-3 d-sm-flex justify-content-sm-center justify-content-xl-start">
                                     <a class="btn btn-primary btn-lg px-4 me-sm-3" href="/tees">Tee Time!</a>
