f1a138003d3793c935647adca826e57b44a4909e
El Linzer <elinzer5@gmail.com>
2023-03-01 15:48:54 -0600

    backend endpoints for reviews

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -17,11 +17,58 @@ router.get('/:teeId', async (req, res) => {
 });
 
 //create a review
+router.post('/', requireAuth, async (req, res) => {
+    const { userId, teeId, rating, review } = req.body;
+    const newReview = await Review.create({
+        userId,
+        teeId,
+        rating,
+        review
+    });
+
+    res.json({
+        'Review': newReview
+    })
+
+});
+
 //update a review
+router.put('/:reviewId', requireAuth, async (req, res) => {
+    const { userId, teeId, rating, review } = req.body;
+    const reviewToUpdate = await Review.findByPk(req.params.reviewId);
+
+    if (reviewToUpdate) {
+        await reviewToUpdate.update({
+            userId,
+            teeId,
+            rating,
+            review
+        });
+        res.json({
+            'Review': reviewToUpdate
+        })
+    } else {
+        res.json({
+            'Error': 'Review not found'
+        })
+    }
+});
+
 //delete a review
+router.delete('/:reviewId', requireAuth, async (req, res) => {
+    const reviewToDelete = await Review.findByPk(req.params.reviewId);
 
-
-
+    if (reviewToDelete) {
+        await reviewToDelete.destroy();
+        res.json({
+            "Message": "Review deleted"
+        })
+    } else {
+        res.json({
+            'Error': 'Review not found'
+        })
+    }
+});
 
 
 
